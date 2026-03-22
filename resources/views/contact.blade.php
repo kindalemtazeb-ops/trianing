1ec8fb19812f22f7f5d020116dc864712fcc586b
Kindalem Tazeb <kindalemtazeb@gmail.com>
2026-03-22 11:51:45 +0300

    auto save working

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,13 +1,21 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact</title>
+    <title>Contact Page</title>
+    <style>
+        body {
+            background-color: goldenrod;
+            text-align: center;
+            font-family: Arial, sans-serif;
+        }
+        h1 {
+            color: blue;
+            margin-top: 50px;
+        }
+    </style>
 </head>
 <body>
-    <h1>Contact Us</h1>
-    <h1>Contact Page</h1>
-    <p>This is the contact page.</p>
+    <h1>CONTACT PAGE!</h1>
+    <p>WELLCOME TO OUR CONTACT PAGE</p>
 </body>
 </html>
